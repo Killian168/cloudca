009b7b97bf1c2cf8cfd981799d9a4870bf86b149
Brendan O\'Daly <odalybr@hotmail.com>
2021-07-30 19:27:29 +0100

    Added get_all_members handler and tests.

---
+++ src/get_all_members/handler.py
@@ -0,0 +1,30 @@
+from ..common.services.lambda_ import Lambda, LambdaResponseCodes
+from ..common.models.member import Member
+from ..common.services.logger import get_logger
+import boto3
+
+MEMBERS_TABLE = 'Members'
+LOGGER = get_logger()
+
+
+# Entry point for getMembersByTeam lambda
+def get_all_members(event, context):
+    # Scan DynamoDb members table for members where teamId==team_id (var `team_id` instantiated above)
+    dynamodb_resource = boto3.resource('dynamodb')
+    table = dynamodb_resource.Table(MEMBERS_TABLE)
+    response = table.scan()
+    LOGGER.debug(f'Table scan responded with: {response}')
+
+    # Create Member objects for each one of the members returned
+    members_list = []
+    for obj in response['Items']:
+        # Unpack obj key, value pairs into Member parameters for example if obj = {"key": "val", "key1": "val1" }
+        # the Member(**obj) would produce: Member(key=value, key1=val1)
+        # Once the Member object is instantiated, append to members_list
+        members_list.append(Member(**obj).dict())
+
+    # Construct response and send back
+    return Lambda.format_response(
+        status_code=LambdaResponseCodes.OK,
+        response_message=members_list
+    )
---
+++ test/test_get_all_members/test_handler.py
@@ -0,0 +1,66 @@
+from unittest import TestCase
+
+from src.get_all_members.handler import get_all_members
+from src.get_members_by_team.handler import get_members_by_team_handler, MEMBERS_TABLE
+from json import dumps
+from moto import mock_dynamodb2
+import boto3
+
+
+class TestHandlerBaseCase(TestCase):
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+
+class TestGetAllMembersHandler(TestHandlerBaseCase):
+
+    @mock_dynamodb2
+    def test_should_return_200_and_members_list(self):
+        # Set up
+        dynamodb_client = boto3.client('dynamodb')
+
+        # Set up dynamoDb Table
+        dynamodb_client.create_table(
+            TableName=MEMBERS_TABLE,
+            KeySchema=[{'AttributeName': 'id', 'KeyType': 'HASH'}],
+            AttributeDefinitions=[{'AttributeName': 'id', 'AttributeType': 'S'}]
+        )
+
+        # Put item in table
+        dynamodb_client.put_item(
+            TableName=MEMBERS_TABLE,
+            Item={
+                'id': {
+                    'S': 'test-id'
+                },
+                'firstName': {
+                    'S': 'test-first-name'
+                },
+                'lastName': {
+                    'S': 'test-last-name'
+                },
+                'teamId': {
+                    'S': 'test_team_id'
+                }
+            }
+        )
+
+        # Call method
+        response = get_all_members(None, None)
+
+        # Assert Behaviour
+        expected_response = {
+            'statusCode': 200,
+            'body': {
+                'message': dumps([{
+                    "id": "test-id",
+                    "firstName": "test-first-name",
+                    "lastName": "test-last-name",
+                    "teamId": "test_team_id"
+                }])
+            }
+        }
+        self.assertEqual(response, expected_response)
