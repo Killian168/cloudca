d8c006fd9a030e0965bfc09f6e4f2c0cd9621888
Killian O'Dalaigh <Killian.ODalaigh@genesys.com>
2021-07-30 11:07:26 +0100

    Init GetMembersByTeam Lambda

---
+++ src/common/services/logger.py
@@ -0,0 +1,13 @@
+import logging
+from pythonjsonlogger import jsonlogger
+
+
+def get_logger():
+    logger = logging.getLogger('cloud-ca')
+    logger.setLevel(logging.DEBUG)
+
+    stdout_handler = logging.StreamHandler()
+    stdout_handler.setFormatter(jsonlogger.JsonFormatter())
+
+    logger.addHandler(stdout_handler)
+    return logger
