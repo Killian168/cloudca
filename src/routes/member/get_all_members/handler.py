d3270356f10bb11a00aa304703b497fa917c7d81
Killian168 <odalyki@outlook.com>
2021-12-06 22:10:50 +0000

    add code coverage

--- src/routes/member/get_all_members/handler.py
+++ src/routes/member/get_all_members/handler.py
@@ -1,8 +1,7 @@
-import boto3
-
 from src.common.constants import MEMBERS_TABLE_NAME
 from src.common.enums.api_response_codes import APIResponseCodes
 from src.common.models.member import Member
+from src.common.services.dynamodb import DynamoDB
 from src.common.services.lambda_ import Lambda
 from src.common.services.logger import get_logger
 
@@ -10,13 +9,11 @@ LOGGER = get_logger()
 
 
 def get_all_members(event, context):
-    dynamodb_resource = boto3.resource("dynamodb")
-    table = dynamodb_resource.Table(MEMBERS_TABLE_NAME)
-    response = table.scan()
-    LOGGER.debug(f"Table scan responded with: {response}")
+    dynamodb = DynamoDB(logger=LOGGER)
+    items = dynamodb.scan_table(table_name=MEMBERS_TABLE_NAME)
 
     members_list = []
-    for obj in response["Items"]:
+    for obj in items:
         members_list.append(Member(**obj).dict())
 
     return Lambda.format_response(status_code=APIResponseCodes.OK, response_message=members_list)
--- src/routes/team/get_all_teams/handler.py
+++ src/routes/team/get_all_teams/handler.py
@@ -1,8 +1,7 @@
-import boto3
-
 from src.common.constants import TEAM_TABLE_NAME
 from src.common.enums.api_response_codes import APIResponseCodes
 from src.common.models.team import Team
+from src.common.services.dynamodb import DynamoDB
 from src.common.services.lambda_ import Lambda
 from src.common.services.logger import get_logger
 
@@ -10,12 +9,10 @@ LOGGER = get_logger()
 
 
 def get_all_teams(event, context):
-    dynamodb_resource = boto3.resource("dynamodb")
-    table = dynamodb_resource.Table(TEAM_TABLE_NAME)
-    response = table.scan()
-    LOGGER.debug(f"Table scan responded with: {response}")
+    dynamodb = DynamoDB(logger=LOGGER)
+    items = dynamodb.scan_table(table_name=TEAM_TABLE_NAME)
 
     members_list = []
-    for obj in response["Items"]:
+    for obj in items:
         members_list.append(Team(**obj).dict())
     return Lambda.format_response(status_code=APIResponseCodes.OK, response_message=members_list)
