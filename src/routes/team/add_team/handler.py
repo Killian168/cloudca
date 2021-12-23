cf68c173e00156d97fd434cf2fca8ff301d81e94
Killian168 <odalyki@outlook.com>
2021-12-23 12:04:36 +0000

    add team route

---
+++ src/routes/team/add_team/handler.py
@@ -0,0 +1,43 @@
+from pydantic import ValidationError
+
+from src.common.constants import TEAM_TABLE_NAME
+from src.common.enums.api_response_codes import APIResponseCodes
+from src.common.models.team import Team
+from src.common.services.dynamodb import DynamoDB
+from src.common.services.lambda_ import Lambda
+from src.common.services.logger import get_logger
+
+LOGGER = get_logger()
+
+
+def add_team(event, context):
+    team_details = event.get("Team", None)
+
+    if team_details is None:
+        error_message = "Event processed does not have key `Team` or object is empty"
+        LOGGER.error(error_message)
+        return Lambda.format_response(
+            status_code=APIResponseCodes.BAD_REQUEST, error_message=error_message
+        )
+
+    try:
+        team = Team(**team_details)
+    except ValidationError as e:
+        error_message = {}
+        for err in e.errors():
+            if error_message.get(err["msg"], None) is None:
+                error_message[err["msg"]] = []
+            if len(err["loc"]) > 1:
+                error_message[err["msg"]].append(dict([err["loc"]]))
+            elif "id" not in err["loc"][0]:
+                error_message[err["msg"]].append(err["loc"][0])
+
+        LOGGER.error(error_message)
+        return Lambda.format_response(
+            status_code=APIResponseCodes.BAD_REQUEST, error_message=error_message
+        )
+
+    dynamodb = DynamoDB(logger=LOGGER)
+    dynamodb.put_item(table_name=TEAM_TABLE_NAME, item=team.dict())
+
+    return Lambda.format_response(status_code=APIResponseCodes.OK, response_message=team.dict())
--- test/routes/team/test_add_team/test_handler.py
+++ test/routes/team/test_add_team/test_handler.py
@@ -4,38 +4,35 @@ from unittest.mock import patch
 
 from parameterized import parameterized
 
-from src.common.constants import MEMBERS_TABLE_NAME
+from src.common.constants import TEAM_TABLE_NAME
 from src.common.enums.api_response_codes import APIResponseCodes
 from src.common.services.lambda_ import Lambda
-from src.routes.member.add_member.handler import add_member
+from src.routes.team.add_team.handler import add_team
 
 
-class TestAddMembersHandler(BaseTestCase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
+class TestAddTeamHandler(BaseTestCase):
     @classmethod
     def setUpClass(cls, *args, **kwargs):
-        super().setUpClass(dynamo_tables=[MEMBERS_TABLE_NAME])
+        super().setUpClass(dynamo_tables=[TEAM_TABLE_NAME])
 
-    def test_should_return_200_and_members_list(self):
+    def test_should_return_200_and_team(self):
         # Set up
         test_uuid = "16ba49bc-5464-4ee2-9846-77264b3ac62d"
 
         # Call method
-        with patch("src.common.models.member.uuid4") as mock_uuid4:
+        with patch("src.common.models.team.uuid4") as mock_uuid4:
             mock_uuid4.return_value = test_uuid
-            response = add_member({"Member": Fixtures.get_member_no_id()}, None)
+            response = add_team({"Team": Fixtures.get_team_no_id_json()}, None)
 
         # Assert Behaviour
         expected_response = Lambda.format_response(
             status_code=APIResponseCodes.OK,
-            response_message=Fixtures.get_player_json(test_uuid),
+            response_message=Fixtures.get_team_json(test_uuid),
         )
         self.assertEqual(response, expected_response)
 
         response = self.dynamodb_client.scan(
-            TableName=MEMBERS_TABLE_NAME,
+            TableName=TEAM_TABLE_NAME,
             ExpressionAttributeValues={
                 ":id": {
                     "S": test_uuid,
@@ -50,33 +47,23 @@ class TestAddMembersHandler(BaseTestCase):
             (
                 "Empty Event",
                 {},
-                "Event processed does not have key `Member`.",
+                "Event processed does not have key `Team` or object is empty",
             ),
             (
-                "Member is None",
-                {"Member": None},
-                "Member can not be: None",
+                "Team is None",
+                {"Team": None},
+                "Event processed does not have key `Team` or object is empty",
             ),
             (
-                "Missing Details",
-                {"Member": {}},
-                "Member does not have a role.",
-            ),
-            (
-                "Invalid Member",
-                {"Member": {"details": {}}},
-                "Member does not have a role.",
-            ),
-            (
-                "No Role Member",
-                {"Member": Fixtures.get_member_no_role_json()},
-                "Member does not have a role.",
+                "Invalid Team",
+                {"Team": {}},
+                {"field required": ["name"]},
             ),
         ]
     )
     def test_should_return_400_and_error_message_on_bad_request(self, name, event, error_message):
         # Call method
-        response = add_member(event, None)
+        response = add_team(event, None)
 
         # Assert Behaviour
         expected_response = Lambda.format_response(
@@ -85,5 +72,5 @@ class TestAddMembersHandler(BaseTestCase):
         )
         self.assertEqual(response, expected_response)
 
-        response = self.dynamodb_client.scan(TableName=MEMBERS_TABLE_NAME)
+        response = self.dynamodb_client.scan(TableName=TEAM_TABLE_NAME)
         self.assertEqual(len(response["Items"]), 0)
