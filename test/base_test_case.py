fab4475cef944fa442ad19a86de162643c484e9d
Killian168 <odalyki@outlook.com>
2021-12-24 18:38:55 +0000

    aws credentials mocking

--- test/base_test_case.py
+++ test/base_test_case.py
@@ -6,13 +6,13 @@ import boto3
 from moto import mock_dynamodb2, mock_s3
 
 
-def aws_credentials():
+def set_up_aws_credentials():
     """Mocked AWS Credentials for moto."""
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
-    os.environ["AWS_REGION"] = "us-east-1"
+    os.environ["AWS_DEFAULT_REGION"] = "us-west-2"
 
 
 class BaseTestCase(TestCase):
@@ -31,7 +31,7 @@ class BaseTestCase(TestCase):
 
     @classmethod
     def setUpClass(cls, dynamo_tables=None, s3_buckets=None, should_log=False):
-        aws_credentials()
+        set_up_aws_credentials()
         cls.maxDiff = None
         cls.dynamo_tables = dynamo_tables
         cls.s3_buckets = s3_buckets
