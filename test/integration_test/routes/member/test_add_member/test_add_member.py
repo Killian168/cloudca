0896b3091e24d6d636e043e60ff00e4c87ec50a6
Killian168 <odalyki@outlook.com>
2022-05-10 00:28:43 +0100

    Fix tests

--- test/integration_test/routes/member/test_add_member/test_add_member.py
+++ test/integration_test/routes/member/test_add_member/test_add_member.py
@@ -3,6 +3,7 @@ from test.test_fixtures.fixtures import Fixtures
 
 MEMBER_RESOURCE = "member"
 
+
 def test_add_member(get_api_url):
     url = get_api_url(resource=MEMBER_RESOURCE, route="addMember")
     member = Fixtures.get_member_no_id()
